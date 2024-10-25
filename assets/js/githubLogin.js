d1f7e709abd6782edf78f48667f3025892104759
Mishell-A <mishellarroyo2@gmail.com>
2024-10-24 19:50:47 -0500

    fix: cambiando toast de error

--- assets/js/githubLogin.js
+++ assets/js/githubLogin.js
@@ -26,7 +26,7 @@ githubButton.addEventListener("click", async (event) => {
   } catch (error) {
     console.log(error);
     if (error.code === "auth/account-exists-with-different-credential") {
-      showMessage("Cuenta ya registrada, iniciar sesi", "error");
+      showMessage("Cuenta ya registrada, iniciar sesión con Google", "error");
     } else {
       showMessage(error.message, "error");
     }
